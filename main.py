34a32b0ada3384410941e272c99baf0225ebab34
kumarraj <kumarraj@gmail.com>
2022-11-20 23:20:42 +0530

    locally run

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
 from sqlalchemy import Table, Column, Integer, ForeignKey
+import os
 
 
 
@@ -23,12 +24,12 @@ def admin_only(f):
     return decorated_function
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY",'8BYkEfBA6O6donzWlSihBXox7C0sKR6b')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL","sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
